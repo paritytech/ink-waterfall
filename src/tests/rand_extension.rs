13b81cd7fd2df3ad416bcc6d50989600491fd5b5
Michael Mueller <mich@elmueller.net>
2021-05-28 20:51:33 +0200

    Fix comments

--- src/tests/rand_extension.rs
+++ src/tests/rand_extension.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Tests for the `flipper `example.
+//! Tests for the `rand-extension `example.
 
 use crate::utils::{
     self,
