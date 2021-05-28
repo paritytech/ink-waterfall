0b35f210ed48f778963e5e170817d36ffc664d8d
Michael Mueller <mich@elmueller.net>
2021-05-28 20:49:30 +0200

    Add ERC-20 tests for allowances

--- src/tests/erc20.rs
+++ src/tests/erc20.rs
@@ -77,3 +77,114 @@ async fn erc20(mut canvas_ui: CanvasUi) -> Result<()> {
 
     Ok(())
 }
+
+#[waterfall_test]
+async fn erc20_allowances(mut canvas_ui: CanvasUi) -> Result<()> {
+    // given
+    let manifest_path = utils::example_path("erc20/Cargo.toml");
+    let contract_file =
+        cargo_contract::build(&manifest_path).expect("contract build failed");
+
+    let contract_addr = canvas_ui
+        .execute_upload(
+            Upload::new(contract_file)
+                .caller("BOB")
+                .push_initial_value("initialSupply", "1000"),
+        )
+        .await?;
+
+    // Alice tries to transfer tokens on behalf ob Bob
+    // TODO
+    //   Has to be ignored until https://github.com/paritytech/ink/issues/641 makes `Result::Err`
+    //   visible in the UI.
+    assert!(
+        true || canvas_ui
+            .execute_transaction(
+                Call::new(&contract_addr, "transfer_from")
+                    .caller("ALICE")
+                    .push_value("from: AccountId", "BOB")
+                    .push_value("to: AccountId", "ALICE")
+                    .push_value("value", "400"),
+            )
+            .await
+            .is_err()
+    );
+
+    // Bob approves Alice being able to withdraw up the `value` amount on his behalf.
+    canvas_ui
+        .execute_transaction(
+            Call::new(&contract_addr, "approve")
+                .caller("BOB")
+                .push_value("spender", "ALICE")
+                .push_value("value", "600"),
+        )
+        .await
+        .expect("`approve` must succeed");
+    assert_eq!(
+        canvas_ui
+            .execute_rpc(
+                Call::new(&contract_addr, "allowance")
+                    .push_value("owner", "BOB")
+                    .push_value("spender", "ALICE")
+            )
+            .await?,
+        "600000000000000"
+    );
+
+    // Alice tries again to transfer tokens on behalf ob Bob
+    canvas_ui
+        .execute_transaction(
+            Call::new(&contract_addr, "transfer_from")
+                .caller("ALICE")
+                .push_value("from: AccountId", "BOB")
+                .push_value("to: AccountId", "ALICE")
+                .push_value("value", "400"),
+        )
+        .await
+        .expect("second `transfer_from` must succeed");
+    assert_eq!(
+        canvas_ui
+            .execute_rpc(
+                Call::new(&contract_addr, "balance_of").push_value("owner", "ALICE")
+            )
+            .await?,
+        "400000000000000"
+    );
+    assert_eq!(
+        canvas_ui
+            .execute_rpc(
+                Call::new(&contract_addr, "balance_of").push_value("owner", "BOB")
+            )
+            .await?,
+        "600000000000000"
+    );
+
+    // Alice tries to transfer even more tokens on behalf ob Bob, this time exhausting the allowance
+    // TODO
+    //   Has to be ignored until https://github.com/paritytech/ink/issues/641 makes `Result::Err`
+    //   visible in the UI.
+    assert!(
+        true || canvas_ui
+            .execute_transaction(
+                Call::new(&contract_addr, "transfer_from")
+                    .caller("ALICE")
+                    .push_value("from: AccountId", "BOB")
+                    .push_value("to: AccountId", "ALICE")
+                    .push_value("value", "201"),
+            )
+            .await
+            .is_err()
+    );
+
+    // Balance of Bob must have stayed the same
+    assert_eq!(
+        canvas_ui
+            .execute_rpc(
+                Call::new(&contract_addr, "balance_of").push_value("owner", "BOB")
+            )
+            .await?,
+        "600000000000000"
+    );
+
+    Ok(())
+}
