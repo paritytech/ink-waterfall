703b2ce8bc91ce20475a8f3bf357fbb29829751f
Michael Mueller <mich@elmueller.net>
2021-06-17 20:58:02 +0200

    Add test for multisig with value transfer transaction

--- src/tests/multisig_plain.rs
+++ src/tests/multisig_plain.rs
@@ -112,3 +112,78 @@ async fn multisig_works_with_flipper_transaction(mut canvas_ui: CanvasUi) -> Res
 
     Ok(())
 }
+
+#[waterfall_test]
+async fn multisig_works_with_payable_transaction(mut canvas_ui: CanvasUi) -> Result<()> {
+    // given
+    let manifest_path = utils::example_path("contract-transfer/Cargo.toml");
+    let contract_file =
+        cargo_contract::build(&manifest_path).expect("contract build failed");
+    let contract_transfer_addr =
+        canvas_ui.execute_upload(Upload::new(contract_file)).await?;
+
+    let manifest_path = utils::example_path("multisig_plain/Cargo.toml");
+    let contract_file =
+        cargo_contract::build(&manifest_path).expect("contract build failed");
+
+    let contract_addr = canvas_ui
+        .execute_upload(
+            Upload::new(contract_file)
+                .push_initial_value("requirement", "2")
+                .add_item("owner", "ALICE")
+                .add_item("owner", "BOB")
+                .add_item("owner", "EVE"),
+        )
+        .await?;
+
+    canvas_ui
+        .execute_transaction(
+            Call::new(&contract_addr, "submit_transaction")
+                .caller("ALICE")
+                .push_value("callee", &contract_transfer_addr)
+                .push_value("selector", "0xcafebabe") // `was_it_ten`
+                .push_value("input", "0x00")
+                .push_value("transferred_value", "123")
+                .push_value("gas_limit", "9999999000")
+                .max_gas("1199999"),
+        )
+        .await
+        .expect("failed to execute `transfer` to BOB transaction");
+    let id = "0";
+    canvas_ui
+        .execute_transaction(
+            Call::new(&contract_addr, "confirm_transaction")
+                .caller("ALICE")
+                .push_value("transId", id)
+                .max_gas("60000"),
+        )
+        .await
+        .expect("failed to execute `transfer` to BOB transaction");
+
+    canvas_ui
+        .execute_transaction(
+            Call::new(&contract_addr, "confirm_transaction")
+                .caller("BOB")
+                .push_value("transId", id)
+                .max_gas("60000"),
+        )
+        .await
+        .expect("failed to execute `transfer` to BOB transaction");
+
+    // when
+    canvas_ui
+        .execute_transaction(
+            Call::new(&contract_addr, "invoke_transaction")
+                .caller("ALICE")
+                .push_value("transId", id)
+                .payment("123", "pico")
+                .max_gas("90000"),
+        )
+        .await
+        .expect("failed to execute `transfer` to BOB transaction");
+
+    // then
+    assert!(utils::canvas_log_contains("received payment: 10"));
+
+    Ok(())
+}
