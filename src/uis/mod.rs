cb92d8aa87791f09beb45fc31d62979b6683e46b
Michael Mueller <mich@elmueller.net>
2021-06-29 04:53:44 +0200

    Add features to control which UI is used

--- src/uis/mod.rs
+++ src/uis/mod.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(feature = "canvas-ui")]
 pub mod canvas_ui;
 
 use async_trait::async_trait;
