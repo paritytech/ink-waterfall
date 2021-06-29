79941bbaf631b577c974816a2bd86417c836e987
Michael Mueller <mich@elmueller.net>
2021-06-29 18:40:01 +0200

    Revert "Revert me: Try if CI runs without shutdown"
    This reverts commit dc327258a4c3a38a0b63a4ded466e5378d068fe9.

--- src/uis/mod.rs
+++ src/uis/mod.rs
@@ -116,6 +116,8 @@ impl Ui {
             );
             return Ok(())
         }
+        log::info!("closing client");
+        self.client.close().await?;
         Ok(())
     }
 }
@@ -128,6 +130,14 @@ impl Drop for Ui {
             );
             return
         }
+        // We kill the `geckodriver` instance here and not in `Ui::shutdown()`.
+        // The reason is that if a test fails (e.g. due to an assertion), then the test
+        // will be interrupted and the shutdown method at the end of a test will not
+        // be reached, but this drop will.
+        log::info!("killing geckodriver");
+        self.geckodriver
+            .kill()
+            .expect("unable to kill geckodriver, it probably wasn't running");
     }
 }
 
