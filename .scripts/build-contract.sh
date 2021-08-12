f94f798d24fbe1cdb7f17421e05d6d5c0c1396ae
Hernando Castano <hernando@hcastano.com>
2021-08-12 11:25:58 -0400

    Try caller script another way

--- .scripts/build-contract.sh
+++ .scripts/build-contract.sh
@@ -4,5 +4,5 @@ set -eux
 
 # TODO: Use `--output-json` when that gets merged
 SIZE_OUT=""
-SIZE_OUT=$(cargo contract build --manifest-path examples/$1/Cargo.toml  | grep "Original" | cut -d " " -f 4,6)
+SIZE_OUT=$(cargo contract build --manifest-path ./ink/examples/$1/Cargo.toml  | grep "Original" | cut -d " " -f 4,6)
 echo "$1, ${SIZE_OUT}" >> output.csv
