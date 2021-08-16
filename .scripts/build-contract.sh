00e26f029f0c4add6e2acf3d20e37ada42b2f272
Hernando Castano <hernando@hcastano.com>
2021-08-16 11:44:10 -0500

    Add `artifacts:name` field

--- .scripts/build-contract.sh
+++ .scripts/build-contract.sh
@@ -6,3 +6,5 @@ SIZE_OUT=$(cargo contract build --manifest-path $1/Cargo.toml --output-json)
 ORIGINAL_SIZE=$(echo $SIZE_OUT | jq '.optimization_result.original_size')
 OPTIMIZED_SIZE=$(echo $SIZE_OUT | jq '.optimization_result.optimized_size')
 echo "$1, ${ORIGINAL_SIZE}, ${OPTIMIZED_SIZE}" >> $2.csv
+
+ls -lah $2.csv
