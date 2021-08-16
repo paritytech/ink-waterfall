16843274552ee95fd7018828cb5d49d2d9e6fce5
Hernando Castano <hernando@hcastano.com>
2021-08-16 11:23:19 -0500

    Make output file name an argument into script

--- .scripts/build-contract.sh
+++ .scripts/build-contract.sh
@@ -5,4 +5,4 @@ set -eux
 # TODO: Use `--output-json` when that gets merged
 SIZE_OUT=""
 SIZE_OUT=$(cargo contract build --manifest-path $1/Cargo.toml  | grep "Original" | cut -d " " -f 4,6)
-echo "$1, ${SIZE_OUT}" >> output.csv #${BRANCH}.csv
+echo "$1, ${SIZE_OUT}" >> $2.csv
