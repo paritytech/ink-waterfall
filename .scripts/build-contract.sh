eb8a0a43f13f6bab4bfd73991d0fed70a9b2596e
Hernando Castano <hernando@hcastano.com>
2021-08-17 10:50:24 -0500

    Extract example contract name when writing to CSV

--- .scripts/build-contract.sh
+++ .scripts/build-contract.sh
@@ -2,7 +2,8 @@
 
 set -eux
 
+CONTRACT=$(basename $1)
 SIZE_OUT=$(cargo contract build --manifest-path $1/Cargo.toml --output-json)
 ORIGINAL_SIZE=$(echo $SIZE_OUT | jq '.optimization_result.original_size')
 OPTIMIZED_SIZE=$(echo $SIZE_OUT | jq '.optimization_result.optimized_size')
-echo "$1, ${ORIGINAL_SIZE}, ${OPTIMIZED_SIZE}" >> $2.csv
+echo "${CONTRACT}, ${ORIGINAL_SIZE}, ${OPTIMIZED_SIZE}" >> $2.csv
