a55fc804284afd2851d6367ac07b4e62f50b9f18
Vladimir Istyufeev <vladimir@parity.io>
2021-09-24 21:53:44 +0300

    Introduce Redis to track contract sizes

--- .scripts/build-contract.sh
+++ .scripts/build-contract.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-# Write the original and optimized sizes of a Wasm file
-# to a CSV file.
+# Write to the stdout the original and optimized sizes of a Wasm file
+# as a CSV format line.
 #
-# Usage: `./build-contract.sh <path_to_contract> <path_to_output_file>`
+# Usage: `./build-contract.sh <path_to_contract>`
 
 set -eux
 
@@ -11,4 +11,5 @@ CONTRACT=$(basename $1)
 SIZE_OUT=$(cargo contract build --manifest-path $1/Cargo.toml --output-json)
 ORIGINAL_SIZE=$(echo $SIZE_OUT | jq '.optimization_result.original_size')
 OPTIMIZED_SIZE=$(echo $SIZE_OUT | jq '.optimization_result.optimized_size')
-echo "${CONTRACT}, ${ORIGINAL_SIZE}, ${OPTIMIZED_SIZE}" >> $2.csv
+
+echo -n "${CONTRACT}, ${ORIGINAL_SIZE}, ${OPTIMIZED_SIZE}"
