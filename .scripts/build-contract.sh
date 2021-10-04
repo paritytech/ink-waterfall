5f733edac2e5e134bd4d3305b2be47334d8aa816
Hernando Castano <HCastano@users.noreply.github.com>
2021-10-04 16:10:54 -0400

    Track Example Wasm Contract Sizes (#7)
    Co-authored-by: Hernando Castano <HCastano@users.noreply.github.com>
    Co-authored-by: Vladimir Istyufeev <vladimir@parity.io>
    Co-authored-by: Vladimir Istyufeev <me@rcn.me>

---
+++ .scripts/build-contract.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Write to the stdout the original and optimized sizes of a Wasm file
+# as a CSV format line.
+#
+# Usage: `./build-contract.sh <path_to_contract>`
+
+set -eux
+
+CONTRACT=$(basename $1)
+SIZE_OUT=$(cargo contract build --release --manifest-path $1/Cargo.toml --output-json)
+ORIGINAL_SIZE=$(echo $SIZE_OUT | jq '.optimization_result.original_size')
+OPTIMIZED_SIZE=$(echo $SIZE_OUT | jq '.optimization_result.optimized_size')
+
+echo -n "${CONTRACT}, ${ORIGINAL_SIZE}, ${OPTIMIZED_SIZE}"
