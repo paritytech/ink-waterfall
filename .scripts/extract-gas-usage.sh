7f3687007380f24d440f2e4079f670ad8005e559
Michael Mueller <mich@elmueller.net>
2021-11-30 14:56:05 +0100

    Fix `grep`

--- .scripts/extract-gas-usage.sh
+++ .scripts/extract-gas-usage.sh
@@ -11,7 +11,7 @@ set -o pipefail
 EXAMPLE=$(basename $1)
 
 USAGE=$(cat /tmp/waterfall.log |
-  grep "example: " $EXAMPLE ", " |
+  grep "example: $EXAMPLE , " |
   egrep --only-matching "estimated gas for transaction is [0-9]*" |
   egrep --only-matching "[0-9]*" |
   awk '{s+=$1} END {print s}') || exit $?
