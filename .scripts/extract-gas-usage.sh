c18c510c7aef9900b8c67d67507001afd931f0ec
Michael Mueller <mich@elmueller.net>
2021-11-30 14:51:02 +0100

    Fix syntax

--- .scripts/extract-gas-usage.sh
+++ .scripts/extract-gas-usage.sh
@@ -8,10 +8,10 @@
 set -eu
 set -o pipefail
 
-EXAMPLE=$($1)
+EXAMPLE=$(basename $1)
 
 USAGE=$(cat /tmp/waterfall.log |
-  grep "example: " $1 ", " |
+  grep "example: " $EXAMPLE ", " |
   egrep --only-matching "estimated gas for transaction is [0-9]*" |
   egrep --only-matching "[0-9]*" |
   awk '{s+=$1} END {print s}') || exit $?
