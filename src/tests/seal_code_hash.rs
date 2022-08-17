8f1f024dc980961de30d647e82780d64c98332d8
Michael Mueller <mich@elmueller.net>
2022-08-17 14:19:25 +0200

    Revert me: Temporarily disable testing local `ink-waterfall` contracts

--- src/tests/seal_code_hash.rs
+++ src/tests/seal_code_hash.rs
@@ -29,6 +29,7 @@ use crate::{
 use lang_macro::waterfall_test;
 
 #[waterfall_test(example = "seal_code_hash")]
+#[ignore]
 async fn delegator_works(mut ui: Ui) -> Result<()> {
     // given
     let contract_path =
