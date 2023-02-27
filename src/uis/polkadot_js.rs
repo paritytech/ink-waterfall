ec72d4ff3645c06543da6704f66cb4fb81b12f2f
Michael Mueller <mich@elmueller.net>
2023-02-27 15:43:25 +0100

    Increase sleep

--- src/uis/polkadot_js.rs
+++ src/uis/polkadot_js.rs
@@ -364,7 +364,7 @@ impl ContractsUi for crate::uis::Ui {
 
         let mut res;
         for waited in 0..21 {
-            std::thread::sleep(std::time::Duration::from_secs(3));
+            std::thread::sleep(std::time::Duration::from_secs(5));
             res = self.client.find(
                 Locator::XPath("//div[contains(@class, 'ui--Status')]//*/div[@class = 'status' and not(contains(text(),'ready') or contains(text(),'usurped') or contains(text(),'outdated'))]")
             ).await;
